30055cd0e90893f920d887f8ed3096f82bde78be
Igor Rusaleev <igor.rusaleev@list.ru>
2020-04-14 16:36:05 +0300

    Исправил замечания

--- src/test.php
+++ src/test.php
@@ -1,6 +1,7 @@
 <?php
 require_once "../vendor/autoload.php";
+
 $idPerformer = 1;
 $idCustomer = 2;
-$strategy = new Task($idPerformer, $idCustomer);
-assert($strategy->getNextStatus('action_cancel') == Task::STATUS_CANCEL, 'cancel action');
+$strategy = new \TaskForce\Task($idPerformer, $idCustomer);
+assert($strategy->getNextStatus('action_cancel') == \TaskForce\Task::STATUS_CANCEL, 'cancel action');
