49190c6bcfd3ace0883604039f2f2b94208fb45f
Igor Rusaleev <igor.rusaleev@list.ru>
2020-09-12 16:08:30 +0300

    Первый шаг на большом пути

--- frontend/assets/TaskForceAsset.php
+++ frontend/assets/TaskForceAsset.php
@@ -1,15 +1,13 @@
 <?php
 
-
     namespace frontend\assets;
-
 
     use yii\web\AssetBundle;
 
-    class TaskForceAsset extends AssetBundle
-    {
-        public $css = [
-            'css/normalize.css',
-            'css/style.css',
+class TaskForceAsset extends AssetBundle
+{
+    public $css = [
+        'css/normalize.css',
+        'css/style.css',
         ];
-    }
+}
